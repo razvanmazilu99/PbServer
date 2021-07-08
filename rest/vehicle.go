308bf4ffb6a267f946bb2338f372ac8d9b84bff6
razvanmazilu99 <mazilu.razvan.moisil@gmail.com>
2021-07-08 10:42:45 +0300

    PostMyVehicle function

--- rest/vehicle.go
+++ rest/vehicle.go
@@ -2,7 +2,6 @@ package rest
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"problem/entity"
@@ -41,18 +40,36 @@ func PostMyVehicle(rw http.ResponseWriter, r *http.Request) {
 	bodyBytes, err := ioutil.ReadAll(reqBody)
 
 	if err != nil {
-		rw.Write([]byte("Error 1"))
+		rw.Write([]byte("Internal issue 1"))
 		return
 	}
 
-	var vehicle entity.Vehicle
-	//err = json.Unmarshal(bodyBytes, &vehicle)
+	var vehicle map[string]interface{}
+	err = json.Unmarshal(bodyBytes, &vehicle)
 
-	/*if err != nil {
-		rw.Write([]byte("Error 2"))
+	if err != nil {
+		rw.Write([]byte("Internal issue 2"))
 		return
-	}*/
+	}
 
-	fmt.Println(vehicle)
+	vehicleType, ok := vehicle["ype"].(string) //schimbam aici tipul in string ca sa nu punem .(type) la switch
+
+	if !ok {
+		rw.Write([]byte("Internal issue 3"))
+		return
+	}
+
+	switch vehicleType {
+	case "car":
+		rw.Write([]byte("This is a car"))
+	case "bike":
+		rw.Write([]byte("This is a bike"))
+	case "bus":
+		rw.Write([]byte("This is a bus"))
+	default:
+		rw.Write([]byte("Vehicle not known"))
+		return
+	}
+
 	rw.Write(bodyBytes)
 }
